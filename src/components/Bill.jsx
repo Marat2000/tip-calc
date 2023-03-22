768cf1f22f30ce4d93da0b845a0d8854592b1526
Marat Azoyan <marat.azoyan1@gmail.com>
2023-03-22 23:23:38 +0400

    center

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -12,7 +12,7 @@ const {input,setInput, mouseOn,mouseOut,billRef , clicked}=useContext(AppContext
   <>
   <h6>Bill</h6>
   <div ref={billRef} onClick={()=>clicked(billRef)} onMouseEnter={mouseOn} onMouseLeave={mouseOut} className="inputArea"  id="billInput">
-    <img src="images/icon-dollar.svg"/>
+    <img src="images/icon-dollar.svg" alt=""/>
   <input id="bill" placeholder="0" value={input} onChange={e=>setInput(e.target.value)}/>
   </div>
 </>
--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -14,7 +14,7 @@ const {num,setNum , numRef , clicked, mouseOn,mouseOut}=useContext(AppContext)
  <div style={{ color:'tomato', fontSize:'.6rem' , margin:'0' , display:`${num==0?'block':'none'}` }} >can,t be zero</div>
  </div>	
    <div ref={numRef} onClick={()=>clicked(numRef)} onMouseEnter={mouseOn} onMouseLeave={mouseOut}  className="inputArea" id="numInput" style={{ border:`${num==0?'tomato 2px solid':'none'}` }}>
-    <img src="images/icon-person.svg"/>
+    <img src="images/icon-person.svg" alt=""/>
   <input id="num" placeholder="0" value={num} onChange={e=>setNum(e.target.value)}  />
   </div>
 </>
